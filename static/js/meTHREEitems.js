6bcde4965eaaa85e504a68fe53eb55ce0afd509b
Riccardo Soldini <soldocode@users.noreply.github.com>
2016-11-01 07:22:26 +0100

    Create meTHREEitems.js
    defined function:
    - draw_sheet(item)
    - draw_profile(item)
    - draw_component(item)
    - draw_assembly(item)

---
+++ static/js/meTHREEitems.js
@@ -0,0 +1,32 @@
+
+
+function draw_sheet(item)
+    {
+     var meObject = THREE.3Dobject();
+
+     return meObject;
+    };
+    
+
+function draw_profile(item)
+    {
+     var meObject = THREE.3Dobject();
+
+     return meObject;
+    };
+
+
+function draw_component(item)
+    {
+     var meObject = THREE.3Dobject();
+
+     return meObject;
+    };
+    
+function draw_assembly(item)
+    {
+     var meObject = THREE.3Dobject();
+
+     return meObject;
+    };
+    
